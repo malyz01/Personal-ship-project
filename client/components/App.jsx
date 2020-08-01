191e0978aa0217ae58cd50e075bcd05403908a97
malyz <mattuy001@gmail.com>
2020-08-02 11:25:00 +1200

    FIXED: reloading FindClassic will fetch info

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 //I will change to BrowserRouter because I think that this only works with Hash Link
-import {BrowserRouter, Route} from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Nav from './Nav'
 import Home from './Home'
 // Evolution pages (Part1)
@@ -22,55 +22,29 @@ import Review from './Review'
 import ShipTerms from './ShipTerms'
 
 class App extends React.Component {
-  constructor(props)  {
-    super() 
+  constructor(props) {
+    super()
   }
 
-  render () {
+  render() {
     return (
       <BrowserRouter>
-    
-          <div>
-            
-              <Route path='/' component={Nav}/>
-              <Route exact path='/' component={Home}/>
-
-              {/* Routes for Evolution pages and associated ships */}
-              <Route exact path='/evolutionpart1' component={EvolutionPart1}/>
-              <Route path = '/evolutionpart1/:ship_name' component={FindClassic}/>
-
-              <Route exact path='/evolutionpart2' component={EvolutionPart2}/>
-              <Route exact path='/evolutionpart2/michelangelo' component={Michelangelo}/>
-              <Route exact path='/evolutionpart2/qe2' component={Qe2}/>
-              <Route exact path='/evolutionpart2/fairstar' component={Fairstar}/>
-              <Route exact path='/evolutionpart2/songofnorway' component={SongofNorway}/>
-              <Route exact path='/evolutionpart2/royalvikingsky' component={RoyalVikingSky}/>
-              <Route exact path='/evolutionpart2/allureoftheseas' component={AllureoftheSeas}/>
-
-              {/* Route for Cruise Lines page */}
-              <Route exact path='/cruiselines' component={CruiseLines}/>
-
-              
-              <Route path = '/cruiselines/:cruise_line' component={Review}/>
-              {/* Route below is recommended by Ricky Sullivan from slack, as it passes a param.
-              The problem is that it just doesn't work at this stage, probably because the backend and api needs to be sorted. 
-              <Route path = '/cruiselines/:cruise_line' component={Review}/> */}
-
-              {/* Route for Ship Component that controls the details we are getting for the ship
-              and then returns Cruise Line and Correct Ships in Review Component. */}
-
-              {/* Commented out because this Ship route does not work */}
-              {/* <Route path = '/cruiselines/:cruise_line' component={Ship}/> */}
-
-              {/* Ship Term Route Added */}
-              <Route path = '/cruiselines/:cruise_line/shipterms' component={ShipTerms}/>
-
-              {/* <Route exact path='/booking' component={Booking}/> */}
-              
-            {/* </div> */}
-          </div>  
-        {/* </React.Fragment>   */}
-      {/* </Router>  */}
+        <Switch>
+          <Route exact path='/' component={Nav} />
+          <Route exact path='/' component={Home} />
+          <Route exact path='/evolutionpart1' component={EvolutionPart1} />
+          <Route exact path='/evolutionpart1/:shipname' component={FindClassic} />
+          <Route exact path='/evolutionpart2' component={EvolutionPart2} />
+          <Route exact path='/evolutionpart2/michelangelo' component={Michelangelo} />
+          <Route exact path='/evolutionpart2/qe2' component={Qe2} />
+          <Route exact path='/evolutionpart2/fairstar' component={Fairstar} />
+          <Route exact path='/evolutionpart2/songofnorway' component={SongofNorway} />
+          <Route exact path='/evolutionpart2/royalvikingsky' component={RoyalVikingSky} />
+          <Route exact path='/evolutionpart2/allureoftheseas' component={AllureoftheSeas} />
+          <Route exact path='/cruiselines' component={CruiseLines} />
+          <Route exact path='/cruiselines/:cruise_line' component={Review} />
+          <Route exact path='/cruiselines/:cruise_line/shipterms' component={ShipTerms} />
+        </Switch>
       </BrowserRouter>
     )
   }
--- client/components/FindClassic.jsx
+++ client/components/FindClassic.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
 import MakeClassic from './MakeClassic'
+import { getClassic } from '../api/api'
 
 class FindClassic extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      classicship: false
+    }
+    this.setUpClassic = this.setUpClassic.bind(this)
+  }
+
+  componentDidMount() {
+    const { state } = this.props.location
+    if (state) {
+      this.setState({ classicship: state })
+    } else {
+      this.setUpClassic(state.ship_name)
+        .then(res => {
+          this.setState({ classicship: res })
+        })
+    }
+  }
+
+  setUpClassic(shipName) {
+    getClassic(shipName)
+      .then(res => {
+        this.setState({
+          classicship: res
+        })
+      })
+  }
+
   render() {
+    if (!this.state.classicship) return <div>Loading...</div>
     return <MakeClassic {...this.props.location.state} />
   }
 }
